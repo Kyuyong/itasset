cc928b1b0856fa7040edb918632e4b5ab707d2bf
kyuyong <kyuyong@kyuyongui-Macmini.local>
2023-09-26 15:31:47 +0900

    1st

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { BsSearch } from 'react-icons/bs';
+import MainNavbar from './components/MainNavbar'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container fluid className="main-container">
+        <div className="main-box">
+          <div className="main-bg">
+            <MainNavbar />
+            <div className="mt-5">
+              <Row>
+                <Col md={6}>
+                  <div className="main-text">
+                    <div className="vertical-center">
+                      <p className="main-introtext">SK오앤에스의 모든 AI/DT Solution를 <br></br>
+                        모두 만나볼 수 있습니다.</p>
+                      <p>
+                        python, tensorflow, javascript, django 등의 여러가지 Language를 활용하여 <br></br>
+                        현장에 필요한 Tool 개발하여 제공하고 있습니다.
+                      </p>
+                    </div>
+                    <Form className="d-flex search-form">
+                      <Form.Control
+                        type="search"
+                        placeholder="원하는 검색어를 입력해주세요."
+                        className="me-2"
+                        aria-label="Search"
+                      />
+                      <Button >
+                        <BsSearch size={24} />
+                      </Button>
+                    </Form>
+                  </div>
+                </Col>
+                <Col md={6} className="text-right">
+                  <div className="main-img vertical-center">
+                    <img src={process.env.PUBLIC_URL + "/image/main/MainTitile.png"} alt="title" />
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/MainNavbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { BsFillHouseDoorFill, BsFillPersonFill, BsFillGearFill, BsClipboardFill } from 'react-icons/bs';
+
+function CustomNavbar({ logoText, menuItems, bgColor }) {
+  return (
+    <Navbar bg="transparent" expand="lg" className="main-navbar">
+
+      <Navbar.Brand href="#" className="logo" >IT Asset</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="#" className="d-flex flex-column align-items-center main-nav">
+            <BsFillHouseDoorFill size={24} />
+            <span>Home</span>
+          </Nav.Link>
+          <Nav.Link href="#" className="d-flex flex-column align-items-center main-nav">
+            <BsFillPersonFill size={24} />
+            <span>Work</span>
+          </Nav.Link>
+          <Nav.Link href="#" className="d-flex flex-column align-items-center main-nav">
+            <BsFillGearFill size={24} />
+            <span>Setting</span>
+          </Nav.Link>
+          <Nav.Link href="#" className="d-flex flex-column align-items-center main-nav">
+            <BsClipboardFill size={24} />
+            <span>Login</span>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+
+    </Navbar>
+  );
+}
+
+export default CustomNavbar;
