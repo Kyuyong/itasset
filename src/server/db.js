895eaeb05e9a6038cda55989dd6d70d432f7eab5
조규용 <mvpcouple@naver.com>
2024-02-21 19:02:18 +0900

    V.94 연동실패

--- src/server/db.js
+++ src/server/db.js
@@ -3,8 +3,24 @@ const pool = mysql.createPool({
   host: '172.19.152.27',
   user: 'onsdb_jo',
   password: 'Onsdb123$',
-  database: 'special.TB_itasset_solutiondata',
-  port: 3306
-
+  database: 'TB_Itasset_solutiondata',
+  // port: 3306
 });
+
+
+// 연결 테스트를 위한 함수 추가
+// async function testConnection() {
+//   try {
+//     const connection = await pool.promise().getConnection();
+//     console.log('Database connection successful');
+//     connection.release();
+//   } catch (error) {
+//     console.error('Database connection failed', error);
+//     console.error('Error code:', error.code);
+//   }
+// }
+
+// testConnection();
+
+
 module.exports = pool.promise();
--- src/server/server.js
+++ src/server/server.js
@@ -1,20 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./db')
+
 const app = express();
 const port = process.env.PORT || 3000;
-const db = require('./db.js')
-const bodyParser = require('body-parser');
+// const cors = require('cors');
+// app.use(cors());
 
 
 app.use(bodyParser.json());
 
+// 'RegisterSolution' 컴포넌트에서 전송된 데이터를 처리하는 '/registersolution' 경로
 app.post('/registersolution', async (req, res) => {
-  // 솔루션 등록 로직 구현
+  // 클라이언트에서 전송된 데이터 추출
+  const { sol_id, sol_nm, sol_full_nm, sol_kor_nm, n_id, n_nm, headqt, team, url, github_url, work_fld, search_tag }
+    = req.body;
+
   try {
-    // 데이터 검증 및 데이터베이스 저장 로직
+    // 데이터베이스에 데이터 저장
+    const result = await db.execute(`
+      INSERT INTO SolutionTable (sol_id, sol_nm, sol_full_nm, sol_kor_nm, n_id, n_nm, headqt, team, url, github_url, work_fld, search_tag)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+      [sol_id, sol_nm, sol_full_nm, sol_kor_nm, n_id, n_nm, headqt, team, url, github_url, work_fld, search_tag]
+    );
+    console.log(result); // 결과 로깅, 예: 삽입된 행의 수 확인
+    // 성공 응답 전송
     res.status(200).send('솔루션 등록 성공');
   } catch (error) {
-    res.status(500).send('서버 에러');
+    // 에러 처리
+    console.error('솔루션 등록 중 에러 발생:', error);
+    res.status(500).send('솔루션 등록 실패');
   }
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
